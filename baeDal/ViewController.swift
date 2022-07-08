1cb1efe2703eeb4254b621ab11e462b8a04aab4a
idhun90 <idhun90@naver.com>
2022-07-08 09:22:10 +0900

    Initial Commit

---
+++ baeDal/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  baeDal
+//
+//  Created by 이도헌 on 2022/07/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        print(1)
+        print(2)
+        print(3)
+        
+        print(4)
+        print(5)
+        
+    }
+
+
+}
+
