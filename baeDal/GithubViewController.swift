06b9a7cf42d58daf8d33c846d239e48fed59a5d3
idhun90 <idhun90@naver.com>
2022-07-08 09:48:52 +0900

    기존 뷰 컨트롤러에 코드 추가 및 GithubViewController 파일 생성

---
+++ baeDal/GithubViewController.swift
@@ -0,0 +1,8 @@
+//
+//  GithubViewController.swift
+//  baeDal
+//
+//  Created by 이도헌 on 2022/07/08.
+//
+
+import Foundation
--- baeDal/ViewController.swift
+++ baeDal/ViewController.swift
@@ -12,16 +12,18 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        print(1)
-        print(2)
-        print(3)
+        print(10)
+        print(20)
+        print(30)
         
-        print(4)
+        print(40)
         print(5)
         
         print(6)
         print(7)
         print(8)
+        
+        
         
     }
 
